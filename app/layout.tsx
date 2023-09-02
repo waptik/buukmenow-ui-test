82cf8f0976914588fa0a32c66b618fa4bbacd9b0
waptik <itswaptik@gmail.com>
2023-09-02 20:23:34 +0000

    playing with layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter,  } from "next/font/google";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex flex-column justify-center min-h-[1000px] bg-gray-100 w-full">
-          <div className="overflow-hidden w-[1440px] h-full border">
+        <div className="flex flex-col w-full min-h-screen overflow-x-hidden">
+          {/* <div className="overflow-hidden w-[1440px] h-full border"> */}
             {children}
-            <div className="absolute top-[884px] left-[1085px] bg-[#d9d9d9] w-0.5 h-0.5 rounded-1" />
-          </div>
+          {/* </div> */}
         </div>
+        <Toaster />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,23 @@ export default function Page() {
   return (
     <>
       <Menu />
-      <div className="flex w-[939px] gap-[747px] relative my-8 left-[251px]">
-        <div className="inline-flex flex-col items-start gap-4 relative flex-[0_0_auto]">
-          <div className="w-[110px] -mt-px [font-family:'Matter-Medium',_Helvetica] font-medium text-selected text-xl tracking-[-0.40px] relative">
-            Customers
+      <div className="flex items-center">
+        <main className="flex flex-col bg-gray-100 w-full h-auto p-6">
+          <div className="flex w-[939px] gap-[747px] relative my-8 left-[251px]">
+            <div className="inline-flex flex-col items-start gap-4 relative flex-[0_0_auto]">
+              <div className="w-[110px] -mt-px font-medium text-selected text-xl tracking-[-0.40px] relative">
+                Customers
+              </div>
+              <p className="w-fit font-normal text-secondary text-sm tracking-[-0.28px] whitespace-nowrap relative">
+                See all your customers in one place
+              </p>
+            </div>
           </div>
-          <p className="w-fit [font-family:'Matter-Regular',_Helvetica] font-normal text-secondary text-sm tracking-[-0.28px] whitespace-nowrap relative">
-            See all your customers in one place
-          </p>
-        </div>
+          <NavTabs />
+          <CustomerSearch />
+          <CustomersTable />
+        </main>
       </div>
-      <NavTabs />
-      <CustomerSearch />
-      <CustomersTable />
     </>
   );
 }
